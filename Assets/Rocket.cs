fe0f289bbaaa1c9e1e92be19634dfebecd9fb236
Maggie Chu <>
2018-04-29 02:37:30 +0200

    Thursting, Rotation added

---
+++ Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start() {
+        //get reference to Rigid Body
+        rigidBody = GetComponent<Rigidbody>(); //act on components of type Rigidbody
+    }
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
+            print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward); //z axis
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward); //z axis
+        }
+
+    }
+}
