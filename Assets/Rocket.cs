d52ccb52cc9a704fe663955fef346abf98b5ea56
Maggie Chu <>
2018-04-29 03:25:24 +0200

    added audio to code

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Use this for initialization
     void Start() {
         //get reference to Rigid Body
         rigidBody = GetComponent<Rigidbody>(); //act on components of type Rigidbody
+        audioSource = GetComponent<AudioSource>();
     }
 	// Update is called once per frame
 	void Update () {
@@ -18,9 +20,15 @@ public class Rocket : MonoBehaviour {
 	}
 
     private void ProcessInput() {
-        if (Input.GetKey(KeyCode.Space)) { //can thrust while rotating
-            print("Thrusting");
+        if (Input.GetKey(KeyCode.Space)) { //can thrust while rotation
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) { //so it doesn't layer on top of each other
+                audioSource.Play(); 
+            }
+
+        }
+        else {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward); //z axis clockwise
